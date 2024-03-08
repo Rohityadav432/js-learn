f5ad74d8136a76b933561ea26d669308d8e1668d
Rohityadav432 <151834763+Rohityadav432@users.noreply.github.com>
2024-03-08 22:30:33 +0000

    massage

--- basics/variables.js
+++ basics/variables.js
@@ -1,5 +1,14 @@
 const accountId = 123456 
-let accountEmail "rohit@google.com"
+let accountEmail = "rohit@google.com"
 var accountPassword ="123321"
+let accountstate;
+
+// accountId = 32    // not allowed
+accountEmail= "yadav@kk"
+accountPassword="111112"
+// prefer not to use var because of issue in block scope and functional scope
+
 
 console.log(accountId);
+console.table([accountId , accountEmail , accountPassword ,accountstate]); //prints simultaneously 
+
